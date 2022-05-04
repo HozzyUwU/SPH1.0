8efb668e878ff2804039bfdcb80e0cee84900bde
HOZZY <theuniversejohnny@yandex.ru>
2022-05-04 13:20:38 +0300

    nothing special

---
+++ SPHsolver/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <ctime>
+
+#include <GL/glut.h>
+#include "stdlib.h"
+#include "math.h"
+//#include "helper.h"
+//#include "solver.h"
+
+// Definiing debugging variables
+#define FPS_TIME 1 // Seconds
+int simPause = 1; // Paused at start
+
+// Defining program variables
+char* windowTitle; // Pointer for changing fps counter in title each "FPS_TIME" seconds
+float windowWidth = 800.0;
+float windowHeight = 800.0;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
